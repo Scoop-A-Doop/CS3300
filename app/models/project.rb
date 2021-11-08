588aa789430962400660d8f5e8c5e1b9c0f3317c
Scoop-A-Doop <shouibsuleyman@gmail.com>
2021-11-08 03:59:24 +0000

    Init

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     # Makes sure that the description is present for the project, so that the project can be made. 
-    # If the presenece of the description isnt there, then it will fail and the project cant be created
-    validates_presence_of :description
+    # If the presenece of the title and description isnt there, then it will fail and the project cant be created
+    validates_presence_of :title, :description
 end

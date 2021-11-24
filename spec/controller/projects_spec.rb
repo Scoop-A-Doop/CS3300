6ba7c3b0e35a7999e61e9481edfaac11d2dc96ce
Scoop-A-Doop <shouibsuleyman@gmail.com>
2021-11-24 06:13:57 +0000

    Init

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -1,20 +1,69 @@
-require "rails_helper"
+require 'rails_helper'
 
-# Make sure that the controller is responding to the browser successfully. Otherwise it can throw an error
-RSpec.describe ProjectsController, type: :controller do
-  context "GET #index" do
-    it "returns a success response" do
-      get :index
-      # expect(response.success).to eq(true)
-      expect(response).to be_success
+# Test if the project can take a Title
+RSpec.feature "Projects", type: :feature do
+  context "Create new project" do
+    before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit new_project_path
+      within("form") do
+        fill_in "Title", with: "Test title"
+      end
+    end
+
+    # If successful, Test if a description can be added
+    scenario "should be successful" do
+      fill_in "Description", with: "Test description"
+      click_button "Create Project"
+      expect(page).to have_content("Project was successfully created")
+    end
+
+    # If fail, then expect an error message
+    scenario "should fail" do
+      click_button "Create Project"
+      expect(page).to have_content("Description can't be blank")
     end
   end
 
-  context "GET #show" do
-    let!(:project) { Project.create(title: "Test title", description: "Test description") }
-    it "returns a success response" do
-      get :show, params: { id: project }
-      expect(response).to be_success
+  # Test an update to a project
+  context "Update project" do
+    let(:project) { Project.create(title: "Test title", description: "Test content") }
+    before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit edit_project_path(project)
+    end
+
+    # Success expects a successful message
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Description", with: "New description content"
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Project was successfully updated")
+    end
+
+    # Fail fills the description with an empty string and expects an error message
+    scenario "should fail" do
+      within("form") do
+        fill_in "Description", with: ""
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Description can't be blank")
     end
   end
-end 
+
+  # Checks a project to see if it's available to be destroyed/ deleted
+  context "Remove existing project" do
+    let!(:project) { Project.create(title: "Test title", description: "Test content") }
+    scenario "remove project" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit projects_path
+      click_link "Destroy"
+      expect(page).to have_content("Project was successfully destroyed")
+      expect(Project.count).to eq(0)
+    end
+  end
+end

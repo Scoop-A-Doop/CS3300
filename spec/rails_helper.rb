93205920b78a12947b4e58f4fd02ec0a0e481cab
Scoop-A-Doop <shouibsuleyman@gmail.com>
2021-11-26 05:31:38 +0000

    Init

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -43,6 +43,7 @@ end
 RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Devise::Test::IntegrationHelpers, :type => :feature
   config.include FactoryBot::Syntax::Methods
   config.extend ControllerMacros, :type => :controller
 
